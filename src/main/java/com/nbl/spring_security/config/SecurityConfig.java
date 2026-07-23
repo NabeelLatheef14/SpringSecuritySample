d9560d306e5443cc4ea7f6fce151711e4fc65d7b
NLatheef <nabeellatheef14@gmai.com>
2026-07-23 10:44:41 +0530

    Multiple user login and authentication phase one.

--- src/main/java/com/nbl/spring_security/config/SecurityConfig.java
+++ src/main/java/com/nbl/spring_security/config/SecurityConfig.java
@@ -1,25 +1,55 @@
 package com.nbl.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
         security.csrf(customizer -> customizer.disable());
         security.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        security.formLogin(Customizer.withDefaults());
+        //security.formLogin(Customizer.withDefaults());
         security.httpBasic(Customizer.withDefaults());
         security.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return security.build();
     }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails admin = User.withDefaultPasswordEncoder().username("nabeel").password("nbl@123").roles("ADMIN").build();
+        UserDetails user = User.withDefaultPasswordEncoder().username("shafna").password("sha@123").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+
+    }*/
 
 }
